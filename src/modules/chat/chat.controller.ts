769815770f615fc6c30d959444917bcfbb08dbc9
Alessandro Mangone BID <alessandro.mangone@bidcompany.it>
2025-01-09 12:50:26 +0100

    fix: import with alias

--- src/modules/chat/chat.controller.ts
+++ src/modules/chat/chat.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Inject, Post } from '@nestjs/common';
 import { NewMessageDto } from './dto/new-message.dto';
-import { BedrockResponse } from 'src/modules/awsbedrock/interfaces/bedrock-response.interface';
+import { BedrockResponse } from '@modules/awsbedrock/interfaces/bedrock-response.interface';
 import { ChatService } from './chat.service';
 import { UseInterceptors } from '@nestjs/common';
 import { TransformInterceptor } from '@middleware/transform.interceptors';
@@ -11,8 +11,8 @@ export class ChatController {
     constructor(private readonly chatService: ChatService) {}
 
     @Post()
+    @HttpCode(200)
     async chat(@Body() messageDto: NewMessageDto): Promise<BedrockResponse> {
         return await this.chatService.chat(messageDto);
     }
-
 }
